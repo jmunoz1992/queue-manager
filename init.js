a477846a5b77d6b08a393baf06958732f6f5ae15
jamunoz <jamunoz@expedia.com>
2019-10-28 15:54:37 -0500

    updated vault init

--- init.js
+++ init.js
@@ -14,12 +14,28 @@ rootCas
 
 require('https').globalAgent.options.ca = rootCas
 
-var vaultService = require('./vault.js')()
+var options = {
+  apiVersion: 'v1', // default
+  endpoint: 'https://vault.ch.lab.stockyard.io:8200',
+  token: '025e456a-2389-2a20-e18c-768fbd11c315'
+}
 
-vaultService.getSecretByKey('APP_ID')
+// get new instance of the client
+var vault = require('node-vault')(options)
+
+var PRIVATE_KEY
+var APP_ID
+var WEBHOOK_SECRET
+
+vault.read('secret/QueueManagerGithubApp')
   .then((result) => {
-    console.log('result ', result)
-  }).catch((err) => {
-    console.log('there was an error ', err)
-    process.exit(1)
+    var data = result.data
+    PRIVATE_KEY = data.PRIVATE_KEY
+    APP_ID = data.APP_ID
+    WEBHOOK_SECRET = data.WEBHOOK_SECRET
+
+    console.log('export PRIVATE_KEY="' + PRIVATE_KEY.trim() + '"')
+    console.log('export APP_ID="' + APP_ID.trim() + '"')
+    console.log('export WEBHOOK_SECRET="' + WEBHOOK_SECRET.trim() + '"')
   })
+  .catch(console.error)
